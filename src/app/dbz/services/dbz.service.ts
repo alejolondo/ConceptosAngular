855ec877dc3930c49fffd783c12e1fc0c1165fb7
Alejandro Londoño <97139311+alejolondo@users.noreply.github.com>
2022-10-04 21:55:08 -0500

    Finalizada la seccion de bases

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@angular/core";
+import { Personaje } from "../interfaces/dbz.interface";
+
+@Injectable()
+export class DbzService {
+    
+    private _personajes: Personaje[] = [
+        {
+          nombre: 'Goku',
+          poder: 14000
+        },
+        {
+          nombre: 'Vegeta',
+          poder: 7500
+        },
+        {
+          nombre: 'Trunks',
+          poder: 2000
+        }
+      ];
+
+      get personajes(): Personaje[]{
+        return [...this._personajes]
+      }
+    
+    
+    constructor(){
+    
+    }
+
+    agregarPersonaje(argumento: Personaje){
+    this._personajes.push(argumento)
+    }
+
+}
